dadba109ad8592858012be128ab5d2ba3e3719c6
doloreel <jarimado@gmail.com>
2016-09-03 13:02:02 +1000

    foundational structure for app

---
+++ app/routes/RoutesContainer.js
@@ -0,0 +1,10 @@
+import { connect } from 'react-redux'
+import Routes from './Routes'
+
+import {
+  fetchDecks,
+  addDeck,
+  addQuestion
+} from '../actions/actions'
+
+// -----------------------------------------------------------------------------
---
+++ app/store/database.js
@@ -0,0 +1,35 @@
+
+var users = [
+    {
+        name: 'some name',
+        age: '34',
+        id: '#'
+    }
+]
+
+var stacks = [
+    {
+        id: '#',
+        userId: '#'
+        stuff: '34',
+    }
+]
+
+var questions = [
+    {
+        id: '#',
+        userId: '#'
+        stuff: '34',
+    }
+]
+
+var answers = [
+    {
+        id: '#',
+        userId: '#'
+        stuff: '34',
+    }
+]
+
+
+default export { users, stacks, questions, answers }
