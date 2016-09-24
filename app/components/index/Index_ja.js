3d41472db5803bf1799240e239dd3fce5a3c52c7
doloreel <jarimado@gmail.com>
2016-09-24 21:08:55 +1000

    added tinder card module

---
+++ app/components/index/Index_ja.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  NavigationExperimental,
+  TouchableOpacity
+} from 'react-native'
+
+import S from '../styles/styles.js';
+
+import _ from 'lodash';
+
+// -----------------------------------------------------------------------------
+
+// reducers.js currently holds the store object
+
+import styles from '../styles'
+import store from '../../store/reducers';
+import RoutesContainer from '../../routes/RoutesContainer';
+import Header from '../shared/Header';
+
+// -----------------------------------------------------------------------------
+
+const BACK = {
+  type: 'pop'
+}
+
+// -----------------------------------------------------------------------------
+
+class Stacks extends Component {
+  render() {
+    return (
+      <View style={S.base}>
+        <TouchableOpacity onPress={()=>this.props._handleNavigate(BACK)}>
+          <Text style={styles.welcome}>
+            Stacks
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// REDUX CONTAINER -------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+
+import {
+  connect
+} from 'react-redux';
+
+// -----------------------------------------------------------------------------
+
+
+var mapStateToProps = (state) => {
+  return {
+    state: state
+  }
+}
+
+var mapDispatchToProps = (dispatch) => {
+  return {
+    doSomething: arg => console.log('nice')
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Stacks)
